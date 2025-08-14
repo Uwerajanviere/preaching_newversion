6f7489fd9bb1a39b65c56ab2eda7884cb1f8fc38
Uwerajanviere <124252344+Uwerajanviere@users.noreply.github.com>
2025-08-14 08:13:44 +0200

    Update firebaseAdmin.ts

--- firebase/firebaseAdmin.ts
+++ firebase/firebaseAdmin.ts
@@ -7,6 +7,9 @@ const serviceAccount = {
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // ✅ fixed for Vercel
 };
+console.log("FIREBASE PROJECT ID:", process.env.FIREBASE_PROJECT_ID);
+console.log("EMAIL:", process.env.FIREBASE_CLIENT_EMAIL);
+console.log("PRIVATE KEY length:", process.env.FIREBASE_PRIVATE_KEY?.length);
 
 const app = !getApps().length ? initializeApp({ credential: cert(serviceAccount) }) : getApps()[0];
 const db = getFirestore(app);
